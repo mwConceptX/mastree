19ad81c34202b699d1b9b1e1ae1a766a62e3b8b3
mwConceptX <mwieja@yahoo.com>
2020-02-25 15:00:20 +0100

    gsap

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 AOS.init();
 
-$('.navbar-collapse a').click(function (e) {
-    $('.navbar-collapse').collapse('toggle');
+$(".navbar-collapse a").click(function(e) {
+  $(".navbar-collapse").collapse("toggle");
 });
-
 
 $(".jumbotron").css({
-    height: $(window).height() + "px"
+  height: $(window).height() + "px"
 });
 
-$(window).on("resize", function () {
-    $(".jumbotron").css({
-        height: $(window).height() + "px"
-    });
+$(window).on("resize", function() {
+  $(".jumbotron").css({
+    height: $(window).height() + "px"
+  });
+});
+
+gsap.from(".letter", {
+  duration: 2,
+  scale: 0.5,
+  opacity: 0,
+  delay: 0.5,
+  stagger: 0.2,
+  ease: "elastic",
+  force3D: true
 });
